f49da84e7a9b247036bcb048673491a81c417e6e
Juan Miguel V. Zamora <151744776+juanmiguelzamora@users.noreply.github.com>
2024-07-16 19:57:25 +0800

    Add files via upload

---
+++ function.js
@@ -0,0 +1,10 @@
+function showMenu() {
+    document.getElementById('menu').style.display = 'block';
+    document.getElementById('menubar').style.display = 'none';
+}
+
+function hideMenu() {
+    document.getElementById('menu').style.display = 'none';
+    document.getElementById('menubar').style.display = 'block';
+}
+
